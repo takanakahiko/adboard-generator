1937618e21aa3e9aa3e7df44391629566d103986
takanakahiko <takanakahiko@gmail.com>
2020-04-30 01:37:30 +0900

    feat: draw canvas from images and color

---
+++ components/ImageSelector.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <p>{{ label }}</p>
+    <b-field v-if="file==null" class>
+      <b-upload v-model="file" drag-drop class>
+        <section class="section full-width">
+          <div class="has-text-centered">
+            <p>
+              <b-icon icon="upload" size="is-large"></b-icon>
+            </p>
+            <p>Drop your files here or click to upload</p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+    <div v-if="value" class="preview">
+      <img :src="value" />
+      <button class="delete is-small" type="button" @click="file = null"></button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import { Chrome } from "vue-color";
+
+// interface Object {
+//   /** The initial value of Object.prototype.constructor is the standard built-in Object constructor. */
+//   constructor: Function;
+//   // some more properties here
+// }
+
+export default Vue.extend({
+  components: {
+    "chrome-picker": Chrome
+  },
+
+  props: {
+    value: String,
+    label: String
+  },
+
+  data() {
+    return {
+      file: null as File | null
+    };
+  },
+
+  watch: {
+    file() {
+      if (this.file) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.$emit("input", e.target.result);
+        };
+        reader.readAsDataURL(this.file);
+      } else {
+        this.$emit("input", null);
+      }
+    }
+  }
+});
+</script>
+
+<style scoped>
+.full-width {
+  width: 100%;
+}
+
+.width400 {
+  width: 400px;
+}
+
+.preview {
+  border-radius: 2px;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.3), 0 4px 8px rgba(0, 0, 0, 0.3);
+}
+</style>
+
+<style>
+.upload.control,
+.upload-draggable {
+  width: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,129 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="columns is-mobile">
-        <card title="Free" icon="github-circle">
-          Open source on
-          <a href="https://github.com/buefy/buefy">GitHub</a>
-        </card>
-
-        <card title="Responsive" icon="cellphone-link">
-          <b class="has-text-grey">Every</b> component is responsive
-        </card>
-
-        <card title="Modern" icon="alert-decagram">
-          Built with
-          <a href="https://vuejs.org/">Vue.js</a> and
-          <a href="http://bulma.io/">Bulma</a>
-        </card>
-
-        <card title="Lightweight" icon="arrange-bring-to-front">
-          No other internal dependency
-        </card>
+  <div>
+    <section class="hero is-light">
+      <div class="hero-body has-text-centered">
+        <img src="~/assets/logo.png" class="logo" />
       </div>
-    </div>
-  </section>
+    </section>
+    <section class="section">
+      <div class="container content is-centered has-text-centered">
+        <p>背景の色を決めましょう</p>
+        <client-only>
+          <chrome-picker v-model="colors" disableAlpha disableFields class="max400"></chrome-picker>
+        </client-only>
+      </div>
+      <div class="container content is-centered has-text-centered">
+        <ImageSelector label="色背景におく画像を選んでください" v-model="image1DataURL" class="max400" />
+      </div>
+      <div class="container content is-centered has-text-centered">
+        <ImageSelector label="白背景におく画像を選んでください" v-model="image2DataURL" class="max400" />
+      </div>
+    </section>
+    <section class="section">
+      <div class="container content is-centered has-text-centered">
+        <p>こんな感じでどうでしょうか？</p>
+        <canvas width="1200" height="800" class="canvas" ref="canvas"></canvas>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script>
-import Card from '~/components/Card'
+<script lang="ts">
+import Vue from "vue";
+import ImageSelector from "~/components/ImageSelector.vue";
+import { Chrome } from "vue-color";
 
-export default {
-  name: 'HomePage',
+export default Vue.extend({
+  name: "HomePage",
 
   components: {
-    Card
+    ImageSelector,
+    "chrome-picker": Chrome
+  },
+
+  data() {
+    return {
+      image1DataURL: null,
+      image2DataURL: null,
+      colors: {
+        rgba: { r: 255, g: 0, b: 0, a: 1 }
+      }
+    } as {
+      image1DataURL: string | null;
+      image2DataURL: string | null;
+      colors: any | null;
+    };
+  },
+
+  watch: {
+    async colors() {
+      await this.draw();
+    },
+    async image1DataURL() {
+      await this.draw();
+    },
+    async image2DataURL() {
+      await this.draw();
+    }
+  },
+
+  methods: {
+    async draw() {
+      const ctx = (this.$refs.canvas as HTMLCanvasElement).getContext("2d");
+      if (!ctx) return;
+      ctx.fillStyle = `rgb(255,255,255)`;
+      ctx.fillRect(0, 0, 1200, 800);
+      if (this.image1DataURL && this.image2DataURL && this.colors) {
+        const image1 = await this.getImage(this.image1DataURL);
+        const image2 = await this.getImage(this.image2DataURL);
+        const colCount = 5;
+        const rowCount = 6;
+        const cellWidth = 1200 / colCount;
+        const cellHeight = 800 / rowCount;
+        ctx.fillStyle = this.colors.hex;
+        for (let i = 0; i <= colCount; i++) {
+          for (let j = 0; j <= rowCount; j++) {
+            const x = cellWidth * i;
+            const y = cellHeight * j;
+            const imageX = x + 10;
+            const imageY = y + 10;
+            const imageW = cellWidth - 20;
+            const imageH = cellHeight - 20;
+            if ((i + j) % 2 == 0) {
+              ctx.fillRect(x, y, cellWidth, cellHeight);
+              ctx.drawImage(image1, imageX, imageY, imageW, imageH);
+            } else {
+              ctx.drawImage(image2, imageX, imageY, imageW, imageH);
+            }
+          }
+        }
+        console.log("書いた");
+      }
+    },
+    async getImage(value: string): Promise<HTMLImageElement> {
+      return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.src = value;
+      });
+    }
   }
-}
+});
 </script>
+
+<style scoped>
+.logo {
+  width: 80%;
+  max-width: 400px;
+}
+
+.max400 {
+  width: 80%;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.canvas {
+  width: 80%;
+}
+</style>
