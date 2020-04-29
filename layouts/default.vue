7c1b2b0bd6021124fdb67e31423fb674f0b4b1ad
takanakahiko <takanakahiko@gmail.com>
2020-04-29 18:31:09 +0900

    style: format

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,9 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
+    <nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
+        <a class="navbar-item" href="/">
+          <img src="~assets/buefy.png" alt="Buefy" height="28">
         </a>
 
         <div class="navbar-burger">
@@ -31,15 +20,10 @@
           General
         </p>
         <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
+          <li v-for="(item, key) of items" :key="key">
+            <nuxt-link :to="item.to" exact-active-class="is-active">
+              <b-icon :icon="item.icon" />
+              {{ item.title }}
             </nuxt-link>
           </li>
         </ul>
